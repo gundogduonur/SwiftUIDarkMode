cbb1eef277a0911a754d22a8cfd5267f26f3f416
gundogduonur <gundogduonur@outlook.com.tr>
2022-03-01 19:01:53 +0300

    darkmode

---
+++ Darkmode/Darkmode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Darkmode
+//
+//  Created by Onur Gündoğdu on 1.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Darkmode/Darkmode/DarkmodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkmodeApp.swift
+//  Darkmode
+//
+//  Created by Onur Gündoğdu on 1.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct DarkmodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
