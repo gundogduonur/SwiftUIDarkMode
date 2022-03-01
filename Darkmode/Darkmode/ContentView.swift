dda58a03ab8e4f0d862f15c15a66a68398aa2488
gundogduonur <gundogduonur@outlook.com.tr>
2022-03-01 19:17:38 +0300

    created group and text

--- Darkmode/Darkmode/ContentView.swift
+++ Darkmode/Darkmode/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ScrollView{
+                VStack(spacing:20){
+                    Text("Primary Text")
+                        .foregroundColor(.primary)
+                    Text("Secondary Text")
+                        .foregroundColor(.secondary)
+                    Text("Black Text")
+                        .foregroundColor(.black)
+                    Text("White Text")
+                        .foregroundColor(.white)
+                    Text("Blue Text")
+                        .foregroundColor(.blue)
+                }
+            }
+            .navigationTitle("Dark Mode")
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group{
+            ContentView().preferredColorScheme(.dark)
+            ContentView().preferredColorScheme(.light)
+        }
+        
     }
 }
